c171281b36d673b650a360271478e81a8dacd533
Paul-Sanchez-Rodriguez <robert.sanchez@vallegrande.edu.pe>
2024-03-09 23:22:49 -0500

    añadiendo CV

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-//alert("Hola este es mi Javascript");
+function prueba(){
+    var enlace = document.createElement('a');
+      enlace.href = './CV/CV_RobertSanchez.pdf'; // Reemplaza 'ruta/al/archivo/cv.pdf' con la ruta a tu archivo CV
+      enlace.download = 'CV_RobertSanchez.pdf'; // Nombre con el que se descargará el archivo
+      document.body.appendChild(enlace);
+      enlace.click();
+      document.body.removeChild(enlace);
+};
 
-let nombre = "Paul";
+document.getElementById("redirectButton").onclick = () =>{
+    window.open('https://drive.google.com/file/d/1SJBW2RO9dWoEz6-uT_XWiB17-2vfdhK-/view', '_blank');
+};
 
-console.log("mi nombre es: " , nombre)
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+};
 
+/* document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("redirectButton").onclick = function() {
+        window.open('https://drive.google.com/file/d/1SJBW2RO9dWoEz6-uT_XWiB17-2vfdhK-/view', '_blank');
+    };
+}); */
